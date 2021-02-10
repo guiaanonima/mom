3bf8b85c9f7950c28a0be37715d136897e62ab90
Willian Jesus da Silva <46569232+Willianjesusdasilva@users.noreply.github.com>
2021-02-10 16:53:38 -0300

    Update bfwj.py

--- bfwj.py
+++ bfwj.py
@@ -8,15 +8,16 @@ url = sys.argv[1]
 file = open(sys.argv[2], "r", encoding="utf8", errors='ignore')
 wordlist = file.readlines()
 
+if sys.argv[4] == 'true':
+    ssl = True
+else:
+    ssl = False
 
 def node_request(senha):
     global url
+    global ssl
     senha = senha.replace("\n", "")
-    try:
-        r = requests.get(f'{url}/{senha}', verify=False, stream=True)
-    except:
-        r = requests.get(f'{url}/{senha}', verify=True, stream=True)
-
+    r = requests.get(f'{url}/{senha}', verify=ssl, stream=True)
     if r.status_code < 400:
         print(f'Url:{url}/{senha} - Status Code :{r.status_code}')
 
