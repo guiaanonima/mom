d8d3c3c89f2116f994271dde4685fcb33d028107
Kaio Gomes <kaiogomes.nave@gmail.com>
2021-02-10 17:07:49 -0300

    just refactoring

--- bfwj.py
+++ bfwj.py
@@ -4,29 +4,32 @@ import sys
 
 
 url = sys.argv[1]
+wordlist = sys.argv[2]
+threads = sys.argv[3]
+ssl = sys.argv[4]
 
-file = open(sys.argv[2], "r", encoding="utf8", errors='ignore')
+file = open(wordlist, "r", encoding="utf8", errors='ignore')
 wordlist = file.readlines()
 
 try:
-    if sys.argv[4] == 'ssl':
+    if ssl == 'ssl':
         ssl = True
 except:
     ssl = False
 
+
 def node_request(senha):
-    global url
-    global ssl
     if senha.startswith("#"):
         pass
     else:
         senha = senha.replace("\n", "")
         r = requests.get(f'{url}/{senha}', verify=ssl, stream=True)
         if r.status_code < 400:
-            print(f'Url:{url}/{senha} - Status Code :{r.status_code}')
+            print(f'Found: {url}/{senha} - {r.status_code}')
+
 
 print('Rodando...')
-pool = ThreadPool(int(sys.argv[3]))
+pool = ThreadPool(int(threads))
 results = pool.map(node_request, wordlist)
 
 pool.close()
