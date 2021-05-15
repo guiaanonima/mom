81a45fa45f46891dbfde1d41e9d4401b68ffe1fa
zNairy <znishikkinishio@gmail.com>
2021-05-15 15:50:37 -0300

    add -mc parameter | including or excluding ports to use

--- mom.py
+++ mom.py
@@ -3,7 +3,7 @@
 from shutil import get_terminal_size
 from progressBar import ProgressBar
 from parserArguments import createSetupParser
-from termcolor import colored
+from rich import print
 from requests import get, packages
 from pathlib import Path
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -11,9 +11,10 @@ from wordlistIterator import WordlistIterator
 
 packages.urllib3.disable_warnings(InsecureRequestWarning) # desativa os warnings chatos quando não se utiliza o protocolo https em específico na requisição #
 
-def cleanPrint(message):
-    columns, lines = get_terminal_size((80, 20)) # numero de colunas e linhas do terminal #
-    print(message + ' ' * (columns-len(message)))
+def cleanPrint(message, colorspace):
+    columns, lines = get_terminal_size() # numero de colunas e linhas do terminal #
+    endspace = ' ' * (columns-len(message)+colorspace*2)
+    print(message + endspace)
 
 def verifyWordlistFile(namefile):
     if(Path(namefile).is_file()):
@@ -27,15 +28,13 @@ def wordlistCounter(args):
     return counter 
 
 def checkBasicNamespaceArguments(parser, namespace):
-    if(namespace.url and namespace.wordlist): # parametros basicos para a busca #
-        if(verifyWordlistFile(namespace.wordlist)):
-            pass
-        else:
-            print(colored(f' * A  wordlist: {namespace.wordlist} não foi encontrada.\n', 'red'))
+    if(namespace.url): # parametros basicos para a busca #
+        if not verifyWordlistFile(namespace.wordlist):
+            print(f'[red] * A  wordlist: {namespace.wordlist} não foi encontrada.[red]\n')
             parser.print_help()
             exit(1)
     else:
-        print(colored(f' * Passe todos os argumentos válidos antes de continuar.\n', 'red'))
+        print(f'[red] * Passe todos os argumentos válidos antes de continuar.[red]\n')
         parser.print_help()
         exit(1)
 
@@ -50,23 +49,26 @@ def openWordlistIterator(args):
     return wordlist # retorna a classe iteradora #
 
 def nodeRequest(directory, args):
+    responses = {200: '[green]', 404  : '[red]'}
+
     if not directory.startswith("#") and directory.strip():
         response = get(f'{args.url}/{directory.strip()}', verify=args.ssl, stream=True)
-        if response.status_code < 400:
-            cleanPrint(f'Found: {args.url}/{directory.strip()} -> {response.status_code}')
+        if str(response.status_code) in args.match.split(','):
+            cleanPrint(f'Found: {args.url}/{directory.strip()} -> {responses.get(response.status_code)}{response.status_code}{responses.get(response.status_code)}', len(responses.get(response.status_code)))
             return True
-        
+
 def fullNodeRequest(directory, args):
+    responses = {200: '[green]', 404: '[red]'}
+    
     if not directory.startswith("#") and directory.strip():
         response = get(f'{args.url}/{directory.strip()}', verify=args.ssl, stream=True)
-        cleanPrint(f'Found: {args.url}/{directory.strip()} -> {response.status_code}')
+        cleanPrint(f'Found: {args.url}/{directory.strip()} -> {responses.get(response.status_code)}{response.status_code}{responses.get(response.status_code)}', len(responses.get(response.status_code)))
         if response.status_code < 400:
             return True
 
 def run(wordlist, args):
     full = {True: fullNodeRequest, False: nodeRequest}
-    pbar = ProgressBar(wordlistCounter(args))
-    counter = 0
+    pbar = ProgressBar(wordlistCounter(args));counter = 0
     try:
         for word in wordlist:
             alive = full.get(args.full)(word, args)
@@ -74,7 +76,7 @@ def run(wordlist, args):
                 counter += 1
             pbar.update()
 
-        cleanPrint(f'{counter}/{wordlistCounter(args)} diretorios encontrados!')
+        cleanPrint(f'[yellow]{counter}/{wordlistCounter(args)} diretorios encontrados![yellow]', len('[yellow]'))
 
     except KeyboardInterrupt:
         print()
