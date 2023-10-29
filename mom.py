9950e8fbe00a2495ab10dd2f8b1a8372103e9b8f
unknown <wjs140897@gmail.com>
2023-10-29 13:26:14 -0300

    Add znairy files

--- mom.py
+++ mom.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
-from multiprocessing.dummy import Pool as ThreadPool
+from shutil import get_terminal_size
+from progressBar import ProgressBar
 from parserArguments import createSetupParser
-from termcolor import colored
+from rich import print
 from requests import get, packages
 from pathlib import Path
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from wordlistIterator import WordlistIterator
-from functools import partial
-from tqdm import tqdm
-
-
-
 
 packages.urllib3.disable_warnings(InsecureRequestWarning) # desativa os warnings chatos quando não se utiliza o protocolo https em específico na requisição #
+
+def cleanPrint(message, colorspace):
+    columns, lines = get_terminal_size() # numero de colunas e linhas do terminal #
+    endspace = ' ' * (columns-len(message)+colorspace*2)
+    print(message + endspace)
 
 def verifyWordlistFile(namefile):
     if(Path(namefile).is_file()):
         return True
-    else:
-        return False
+    return False
 
 def wordlistCounter(args):
     wordlist_count = open(args.wordlist, 'r+')
@@ -28,15 +28,13 @@ def wordlistCounter(args):
     return counter 
 
 def checkBasicNamespaceArguments(parser, namespace):
-    if(namespace.url and namespace.wordlist): # parametros basicos para a busca #
-        if(verifyWordlistFile(namespace.wordlist)):
-            pass
-        else:
-            print(colored(f' * A  wordlist: {namespace.wordlist} não foi encontrada.\n', 'red'))
+    if(namespace.url): # parametros basicos para a busca #
+        if not verifyWordlistFile(namespace.wordlist):
+            print(f'[red] * A  wordlist: {namespace.wordlist} não foi encontrada.[red]\n')
             parser.print_help()
             exit(1)
     else:
-        print(colored(f' * Passe todos os argumentos válidos antes de continuar.\n', 'red'))
+        print(f'[red] * Passe todos os argumentos válidos antes de continuar.[red]\n')
         parser.print_help()
         exit(1)
 
@@ -51,49 +49,41 @@ def openWordlistIterator(args):
     return wordlist # retorna a classe iteradora #
 
 def nodeRequest(directory, args):
-    #global progressBar
+    responses = {200: '[green]', 404  : '[red]'}
+
     if not directory.startswith("#") and directory.strip():
         response = get(f'{args.url}/{directory.strip()}', verify=args.ssl, stream=True)
-        if response.status_code < 400:
-            print(f'Found: {args.url}/{directory.strip()} -> {response.status_code}')
-    #progressBar.update(1)
+        if str(response.status_code) in args.match.split(','):
+            cleanPrint(f'Found: {args.url}/{directory.strip()} -> {responses.get(response.status_code)}{response.status_code}{responses.get(response.status_code)}', len(responses.get(response.status_code)))
+            return True
 
 def fullNodeRequest(directory, args):
-    #global progressBar
+    responses = {200: '[green]', 404: '[red]'}
+    
     if not directory.startswith("#") and directory.strip():
         response = get(f'{args.url}/{directory.strip()}', verify=args.ssl, stream=True)
-        print(f'Found: {args.url}/{directory.strip()} - {response.status_code}')
-    #progressBar.update(1)
+        cleanPrint(f'Found: {args.url}/{directory.strip()} -> {responses.get(response.status_code)}{response.status_code}{responses.get(response.status_code)}', len(responses.get(response.status_code)))
+        if response.status_code < 400:
+            return True
 
-def startPool(wordlist, args):
+def run(wordlist, args):
+    full = {True: fullNodeRequest, False: nodeRequest}
+    pbar = ProgressBar(wordlistCounter(args));counter = 0
     try:
-        full = {True: fullNodeRequest, False: nodeRequest}
-        
-        try:
-            pool = ThreadPool(int(args.threads))
-        except ValueError:
-            print(colored(f' Erro, passe o argumento threads em um número inteiro', 'red'))
-            exit(1)
-        pool.map(partial(full.get(args.full), args=args), wordlist) # roda a funcao fazendo requisição para cada diretorio na wordlist iteradora #
-        pool.close()
-        pool.join()
+        for word in wordlist:
+            alive = full.get(args.full)(word, args)
+            if alive:
+                counter += 1
+            pbar.update()
+
+        cleanPrint(f'[yellow]{counter}/{wordlistCounter(args)} diretorios encontrados![yellow]', len('[yellow]'))
 
     except KeyboardInterrupt:
         print()
         exit(0)
 
-
-def main():
-    parser, args = createSetupParser() # cria os parametros e retorna o parser configurado e seus argumentos #
-    checkBasicNamespaceArguments(parser, args) # checa alguns argumentos basicos para rodar o programa #
-    args.url = verifyHttpProtocol(args.url) # verifica se a url informada contem algum dos protocolos web #
-    #progressBar = tqdm(total=wordlistCounter(args))
-    
-    startPool(openWordlistIterator(args), args)
-    
-
-if __name__ == '__main__':
-    print("""
+def screenfetch():
+    return """
 
         888b     d888  .d88888b.  888b     d888
         8888b   d8888 d88P" "Y88b 8888b   d8888
@@ -108,5 +98,16 @@ if __name__ == '__main__':
 by: @canalguiaanonima , @kaio_gomesx , @williansilva.py
                     ----------------
 
-    """)
+    """
+
+def main():
+    print(screenfetch())
+    parser, args = createSetupParser() # cria os parametros e retorna o parser configurado e seus argumentos #
+    checkBasicNamespaceArguments(parser, args) # checa alguns argumentos basicos para rodar o programa #
+    args.url = verifyHttpProtocol(args.url) # verifica se a url informada contem algum dos protocolos web #
+    
+    run(openWordlistIterator(args), args)
+    
+
+if __name__ == '__main__':
     main()
